04430950a9aa6f0eeea545688b2f920671a0077c
Ayush <ayushkansal303@gmail.com>
2024-12-10 13:11:58 +0530

    added backend url

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,3 @@
 // export const BACKEND_URL = "https://anonlabs-backend.vercel.app"
-export const BACKEND_URL = "http://localhost:3000"
+export const BACKEND_URL = "https://anonlabs-backend.vercel.app"
 export const CLIENT_ID="Ov23liNsbw8zvPx7LJa6"
