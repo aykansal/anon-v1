b45604e26fb566c97987edcc78199d9fbac89608
Ayush <ayushkansal303@gmail.com>
2024-12-11 22:47:57 +0530

    fixed web container error

--- frontend/src/hooks/useWebContainer.ts
+++ frontend/src/hooks/useWebContainer.ts
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
 import { WebContainer } from '@webcontainer/api';
-// import { auth } from "@webcontainer/api";
+import { auth } from "@webcontainer/api";
 
 export function useWebContainer() {
     const [webcontainer, setWebcontainer] = useState<WebContainer>();
     async function main() {
-        // auth.init({
-        //     clientId: 'wc_api_raj034_869f9496339c173bb80b713358a16e51',
-        //     scope: '',
-        // });
+        auth.init({
+            clientId: 'wc_api_raj034_869f9496339c173bb80b713358a16e51',
+            scope: '',
+        });
         const webcontainerInstance = await WebContainer.boot();
         setWebcontainer(webcontainerInstance)
     }
