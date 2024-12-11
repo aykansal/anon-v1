7dce0dfb258cfef323e3a8fe76794151d3360362
Ayush <ayushkansal303@gmail.com>
2024-12-11 21:51:06 +0530

    testing webcontainer service

--- frontend/src/hooks/useWebContainer.ts
+++ frontend/src/hooks/useWebContainer.ts
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
 import { WebContainer } from '@webcontainer/api';
+
 import { auth } from "@webcontainer/api";
-
-auth.init({
-    clientId: 'wc_api_raj034_869f9496339c173bb80b713358a16e51',
-    scope: '',
-});
-
 export function useWebContainer() {
+
+    const webContainerAuth = auth.init({
+        clientId: 'wc_api_raj034_869f9496339c173bb80b713358a16e51',
+        scope: '',
+    });
+    console.log("auth: ",webContainerAuth);
+    
     const [webcontainer, setWebcontainer] = useState<WebContainer>();
 
     async function main() {
