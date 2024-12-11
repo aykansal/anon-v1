1aa09690f5a1baac5e0de2e0294797ac1f5ebabb
Ayush <ayushkansal303@gmail.com>
2024-12-11 21:54:17 +0530

    testing cors

--- frontend/src/hooks/useWebContainer.ts
+++ frontend/src/hooks/useWebContainer.ts
@@ -4,16 +4,15 @@ import { WebContainer } from '@webcontainer/api';
 import { auth } from "@webcontainer/api";
 export function useWebContainer() {
 
-    const webContainerAuth = auth.init({
-        clientId: 'wc_api_raj034_869f9496339c173bb80b713358a16e51',
-        scope: '',
-    });
-    console.log("auth: ",webContainerAuth);
-    
     const [webcontainer, setWebcontainer] = useState<WebContainer>();
 
     async function main() {
+        const webContainerAuth = auth.init({
+            clientId: 'wc_api_raj034_869f9496339c173bb80b713358a16e51',
+            scope: '',
+        });
         const webcontainerInstance = await WebContainer.boot();
+        console.log("auth: ", webContainerAuth);
         setWebcontainer(webcontainerInstance)
     }
     useEffect(() => {
