6a8a07df1e269a289fe0bf71f2ff166ff7efd82e
Ayush <ayushkansal303@gmail.com>
2024-12-12 22:02:20 +0530

    test onrender backend

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,3 @@
-export const BACKEND_URL = "https://anonlabs-backend.vercel.app"
-// export const BACKEND_URL = "https://anon-s90p.onrender.com"
+// export const BACKEND_URL = "https://anonlabs-backend.vercel.app"
+export const BACKEND_URL = "https://anon-s90p.onrender.com"
 export const CLIENT_ID = "Ov23liNsbw8zvPx7LJa6"
